c952bfb6ab64611dd620bfea394b9b5fbafe4f89
hemahappy1925 <hemahappy1925@gmail.com>
2017-06-29 12:14:51 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,i,temp=1;
+    scanf("%d",&n);
+    if(i<0)
+    {
+        printf("error");
+    }
+    else
+          for(i=1;i<=n;i++)
+  {
+      temp *= i;
+}
+ printf("factorial of %d",temp);
+return 0;
+}
